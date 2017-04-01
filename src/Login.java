dd414b9c80c1f0578c260ec9d2401ad27bd4fb5a
Abdullah Khan <cmabdullah21@gmail.com>
2017-04-01 23:29:42 +0600

    Database connected

---
+++ src/Login.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import com.mysql.jdbc.Statement;
+
+public class Login {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/AutomationSystem","root","");  
+		//here sonoo is database name, root is username and password  
+		Statement stmt=(Statement) con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from SystemContent");  
+		while(rs.next())  
+	System.out.println(rs.getString(1)+"  "+rs.getString(2));  
+	con.close();  
+	}
+	catch(Exception e){
+		System.out.println(e);
+		}  
+	}  
+}
+
+
